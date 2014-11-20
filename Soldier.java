51ea9f877ef9467119eed6bdc9e487a914da966d
unknown <t00158725@1XXB8CA3A80BCC4.students.ittralee.local>
2014-11-20 14:24:35 +0000

    First Update

---
+++ Soldier.java
@@ -0,0 +1,73 @@
+//Niamh Moylan
+//T00158725
+//Start Date - 19/11/2014
+//OOP2 Project 
+
+//Soldier Class - Base Class
+
+//This class give the blueprint for every soilder created within this game
+
+
+public class Soldier{
+	
+	private String name;
+	private char gender;
+	private String faction;
+	private int regNo;
+
+
+public Soldier (){
+	 this ("Unnamed", x, "Unknown", 0);
+	
+}// end of no-arg
+
+public Soldier (String name, char gender, String faction, int regNo ){
+
+this.name = name;
+this.gender = gender;
+this.faction = faction;
+this.regNo = regNo;
+
+}	//end of multi-arg
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public void setGender (char g) {
+		gender = g;
+	}
+	
+	public void setFaction (String f) {
+		faction = f;
+	}
+	
+	public void setRegNo (int r) {
+		regNo = r;
+	}
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+	public char getGender(){
+		return gender;
+	} 
+	
+	public String getFaction(){
+		return faction;
+	}
+	
+	public int getRegNo(){
+		return regNo;
+	}
+	
+	public String toString() {
+		return String.format("Name %s \n")
+	}
+	
+	
+	
+}// end of class
+
