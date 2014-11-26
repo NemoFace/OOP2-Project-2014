9719cb8b10090bef976d9547cf11e35fc4bfee3f
unknown <t00158725@1XXB8CA3A80BCC4.students.ittralee.local>
2014-11-26 12:04:26 +0000

    Hours waisted over a spelling error

--- MainGUI.java
+++ MainGUI.java
@@ -10,23 +10,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class MainGUI extends JFrame {// implements ActionListener 
+public class MainGUI extends JFrame implements ActionListener{
 
-	
 	JMenu fileMenu;
 	JMenu soldierMenu;
+	JLabel response;
 
-
-	
 	public static void main(String [] args){
-		
 		MainGUI frame = new MainGUI();
 		frame.setVisible(true);
-	}
+	}//main ends
 	
-	
-	
-	//constructor
+
 	public MainGUI(){
 
 		
@@ -35,11 +30,11 @@ public class MainGUI extends JFrame {// implements ActionListener
 		setResizable(false);
 		setLocation(250, 200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+	
 		Container cPane = getContentPane();
 		cPane.setLayout(new FlowLayout());
 		
+		createFileMenu();
 		createSoldierMenu();
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -49,20 +44,31 @@ public class MainGUI extends JFrame {// implements ActionListener
 		menuBar.add(soldierMenu);
 		
 		
-	}
+		response = new JLabel ("Opening of War Game");
+		response.setSize(250,50);
+		cPane.add(response);
+		
+	}//constructer ends
+	
+	public void actionPerformed (ActionEvent event){
+		String menuName;
+		menuName = event.getActionCommand();
+		
+		if (menuName.equals("Quit")){
+			System.exit(0);
+		}
+		else{
+			response.setText("This will open " + menuName);
+		}
+		
+	}//actionPreformed ends
 	
 	
 	private void createSoldierMenu(){
 		
 		JMenuItem item;
 		
-		fileMenu = new JMenu("File");
-		soldierMenu = new JMenu("Book");
-		
-		item = new JMenuItem("Quit");
-		item.setBackground(Color.green);
-		item.addActionListener(this);
-		fileMenu.add(item);
+		soldierMenu = new JMenu("Army");
 		
 		item = new JMenuItem("Add");
 		item.setBackground(Color.red);
@@ -73,6 +79,25 @@ public class MainGUI extends JFrame {// implements ActionListener
 		item.setBackground(Color.blue);
 		item.addActionListener(this);
 		soldierMenu.add(item); 
+	}//soldier menu ends
 	
-	}
-}
+	
+		public void createFileMenu(){
+		JMenuItem item;
+			
+		fileMenu = new JMenu("File");
+			
+		item = new JMenuItem ("History");
+		item.setBackground(Color.yellow);
+		item.addActionListener(this);
+		fileMenu.add(item);
+		
+		item = new JMenuItem("Quit");
+		item.setBackground(Color.green);
+		item.addActionListener(this);
+		fileMenu.add(item);
+		
+		}//file menu ends
+	
+	}//Class ends
+
